38d34a831ef69798cd53eb76c53da5ed024cca1c
luturonunca <nunezcas@iap.fr>
2026-02-08 22:37:00 +0100

    print reassingement

--- pm/mechanical_fine.f90
+++ pm/mechanical_fine.f90
@@ -236,10 +236,10 @@ subroutine mechanical_feedback_fine(ilevel,icount)
               ok=.false.
               bns_sn=.false.
               is_bns_part = is_bns(typep(ipart))
-              if(typep(ipart)%family/=FAM_DM) then
-                 write(*,'(A,1X,I10,1X,I4,1X,L1,1X,ES14.6,1X,ES14.6)') &
+              !if(typep(ipart)%family/=FAM_DM) then
+              write(*,'(A,1X,I10,1X,I4,1X,L1,1X,ES14.6,1X,ES14.6)') &
                       & 'BNS_FAM_CHECK', idp(ipart), typep(ipart)%family, is_bns_part, current_time, t_sn2(ipart)
-              endif
+              !endif
               if(is_bns_part) then
                  bns_sn = t_sn2(ipart) > 0d0 .and. t_sn2(ipart) <= current_time
                  write(*,'(A,1X,I10,1X,I4,1X,ES14.6,1X,ES14.6)') &
